455d9ea74d946870510b92228d9f08a0261d6fe3
Wirtas <wirtekkr@gmail.com>
2021-12-29 22:18:38 +0100

    Weapon update 1
    Added:
    -Gun placeholder
    -Bullet placeholder
    -Basic shooting
    -Destroying active bombs by click

---
+++ Assets/Scripts/Bombullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Bombullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 15f; // Bullet speed
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        if (hitInfo.CompareTag("Player")) return;
+        
+        rb.velocity = Vector3.zero;
+        gameObject.transform.SetParent(hitInfo.transform);
+
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,48 @@
+using System.Linq;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform fireOrigin;
+    [SerializeField] private int maxBombs = 3; // Quantity of possible active bombs
+    private short activeBombsCounter;
+    private GameObject[] activeBombs = new GameObject[3];
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && (activeBombsCounter < maxBombs))
+        {
+            Shoot();
+        }
+
+        if (Input.GetButtonDown("Fire2") && (activeBombsCounter >= 1))
+        {
+            Boom();
+        }
+    }
+
+    private void Shoot()
+    {
+        
+        activeBombs[activeBombsCounter] = Instantiate(bulletPrefab, fireOrigin.position, fireOrigin.rotation);
+        activeBombsCounter++;
+        Debug.Log(activeBombsCounter.ToString());
+    }
+
+    private void Boom()
+    {
+        //TODO potezny wybuch lamiacy duze drzewa i kolyszacy trzcina
+
+
+        if (activeBombsCounter <= 0) return;
+        
+        foreach (GameObject b in activeBombs)
+        {
+           Destroy(b);
+        }
+
+        activeBombsCounter = 0;
+
+    }
+}

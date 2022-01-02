bf1a6b114ae0aaa6d5f8236b78a562ad070ef94c
Wirtas <wirtekkr@gmail.com>
2022-01-02 19:57:04 +0100

    FIX
    boom transfered to weapon script instead of bullet

--- Assets/Scripts/Bombullet.cs
+++ Assets/Scripts/Bombullet.cs
@@ -5,7 +5,7 @@ public class Bombullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed = 15f; // Bullet speed
-    [SerializeField] private ParticleSystem particleSystem;
+
     void Start()
     {
         rb.velocity = transform.forward * speed;
@@ -15,15 +15,9 @@ public class Bombullet : MonoBehaviour
     {
         Debug.Log(hitInfo.name);
         if (hitInfo.CompareTag("Player")) return;
-        
+
         rb.velocity = Vector3.zero;
         gameObject.transform.SetParent(hitInfo.transform);
 
-    }
-
-    private void OnDestroy()
-    {
-        Instantiate(particleSystem, transform.position, transform.rotation);
-        
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform fireOrigin;
+    [SerializeField] private ParticleSystem boom;
     [SerializeField] private int maxBombs = 3; // Quantity of possible active bombs
     private short activeBombsCounter;
     private GameObject[] activeBombs = new GameObject[3];
@@ -32,14 +33,13 @@ public class Weapon : MonoBehaviour
 
     private void Boom()
     {
-        //TODO potezny wybuch lamiacy duze drzewa i kolyszacy trzcina
-        
-
+        //potezny wybuch lamiacy duze drzewa i kolyszacy trzcina
         if (activeBombsCounter <= 0) return;
         
         foreach (GameObject b in activeBombs)
         {
-           Destroy(b);
+            Instantiate(boom, b.transform.position, b.transform.rotation);
+            Destroy(b);
         }
 
         activeBombsCounter = 0;
